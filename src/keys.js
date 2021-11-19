b17ba43c07b1daf6963d2657d5204130693c41e5
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2021-11-19 10:36:37 -0500

    commit 19/11
    tesst  db
    add gitignore

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,17 @@
 module.exports = {
+    // LOCAL
+    // database: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'FisiCalidad',
+    //     database: 'ecoturismo'
+    // }
+
+    // AMBIENTE TEST
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'FisiCalidad',
-        database: 'ecoturismo'
+        host: "buafjdbqroblshuld81t-mysql.services.clever-cloud.com",
+        user: "uqbrgdc9bht3sd2e",
+        password: "U4TsdS5Z7JqYOvgdefH8",
+        database: "buafjdbqroblshuld81t"
     }
 };
