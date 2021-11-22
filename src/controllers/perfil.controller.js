d27e6da74ccbf2fd6fbe51f412e6ae4f4688e869
arnold <luizarnolch@gmai.com>
2021-11-22 13:16:12 -0500

    rolando + arnold

--- src/controllers/perfil.controller.js
+++ src/controllers/perfil.controller.js
@@ -1,8 +1,9 @@
 const pool = require('../database')
+const helpers = require('../library/helpers');
 
 const perfil = {}
 
-perfil.inicio = async (req,res) => {
+perfil.get_inicio = async (req,res) => {
     // console.log(req.user.id_usuario);
     
     const id_usuario = req.user.id_usuario
@@ -18,7 +19,7 @@ perfil.inicio = async (req,res) => {
             LEFT JOIN publicaciones
                 ON perfiles.id_usuario = publicaciones.id_usuario
         WHERE 
-            perfiles.id_usuario = 1;
+            perfiles.id_usuario = ?;
         `
     ,[id_usuario])
     
@@ -26,17 +27,105 @@ perfil.inicio = async (req,res) => {
     //res.render('perfil/perfil')
 }
 
-perfil.cambiar_datos = async (req,res) => {
+perfil.get_cambiar_datos = async (req,res) => {
+    const id_usuario = req.user.id_usuario
+
+    const Perfil = await pool.query(
+        `
+        SELECT
+            *
+        FROM
+            perfiles
+            INNER JOIN usuarios
+                ON perfiles.id_usuario = usuarios.id_usuario
+        WHERE 
+            perfiles.id_usuario = ?;
+        `
+    ,[id_usuario])
+
+    res.render('perfil/perfil_cambiar_datos',{perfil:Perfil[0]})
+}
+
+perfil.post_cambiar_datos = async (req,res) => {
     const id_usuario = req.user.id_usuario
 
     const {
-        
-    } = req.body;
-    res.render('perfil/perfil_actualizar')
+        nombre_perfil,
+        apellido_perfil,
+        email_usuario
+    } = req.body
+
+    const nuevo_perfil = {
+        nombre_perfil,
+        apellido_perfil,
+    }
+
+    await pool(`
+        UPDATE
+            perfiles
+        SET 
+            ?
+        WHERE
+
+            id_usuario = ?
+    `,[nuevo_perfil,id_usuario])
+
+    await pool(`
+        UPDATE
+            usuarios
+        SET 
+            email_usuario = ?
+        WHERE
+            id_usuario = ?
+    `,[email_usuario,id_usuario])
+
+    res.redirect('/perfil')
 }
 
-perfil.cambiar_pass = async (req, res) => {
-    res.render('perfil/perfil_cambiar')
+perfil.get_cambiar_pass = async (req, res) => {
+    res.render('perfil/perfil_cambiar_pass')
+}
+
+perfil.post_cambiar_pass = async (req, res) => {
+    const id_usuario = req.user.id_usuario
+
+    const {
+        pass_usuario,
+        nuevo_pass_usuario,
+        copia_nuevo_pass_usuario
+    } = req.body
+
+    if (nuevo_pass_usuario == copia_nuevo_pass_usuario){
+
+        const rows = await pool.query(`
+            SELECT 
+                *
+            FROM
+                usuarios
+            WHERE
+                id_usuario = ?            
+        `,[id_usuario]);
+
+        const validPassword = await helpers.matchPassword(pass_usuario, rows[0].pass_usuario)
+        pass_usuario = await helpers.encryptPassword(pass_usuario);
+
+        if (validPassword){
+            await pool(`
+            UPDATE
+                usuarios
+            SET 
+                pass_usuario = ?
+            WHERE
+                id_usuario = ?
+        `,[pass_usuario,id_usuario])
+        } else {
+            res.redirect('/perfil')
+        }
+        
+    } else {
+        res.redirect('/perfil')
+    }
+    res.redirect('/perfil')
 }
 
 // TEST BUSCAR ELIMINARME!
--- src/routes/perfil.routes.js
+++ src/routes/perfil.routes.js
@@ -3,11 +3,15 @@ const express = require('express')
 const router = express.Router()
 const perfil = require('../controllers/perfil.controller')
 
-router.get('/', perfil.inicio)
+router.get('/', perfil.get_inicio)
 
-router.get('/cambiar_datos', perfil.cambiar_datos)
+router.get('/cambiar_datos', perfil.get_cambiar_datos)
 
-router.get('/cambiar_pass', perfil.cambiar_pass)
+router.post('/cambiar_datos', perfil.post_cambiar_datos)
+
+router.get('/cambiar_pass', perfil.get_cambiar_pass)
+
+router.post('/cambiar_pass', perfil.get_cambiar_pass)
 
 router.get('/buscar', perfil.buscar)
 
