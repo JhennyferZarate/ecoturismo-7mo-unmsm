aa20da919ece97bed3f749af5cec59def1adc7e8
arnold <luizarnolch@gmai.com>
2021-12-13 16:59:34 -0500

    front + back

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -7,9 +7,9 @@ router.get('/filtro_destinos',admin.get_filtro)
 
 router.post('/filtro_destinos',admin.post_filtro)
 
-router.get('/:id', admin.get_inicio)
+router.get('/destinos/:id', admin.get_inicio)
 
-router.post('/:id', admin.post_inicio)
+router.post('/destinos/:id', admin.post_inicio)
 
 
 
