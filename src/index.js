0294c4600348c2c657db28e0060d6d1940c9c996
arnold <luizarnolch@gmai.com>
2022-01-09 13:33:53 -0500

    listener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 
-const listener = async () => {
-    await console.log('servidor en el puerto: ',app.get('port'))
+const listener = () => {
+    console.log('servidor en el puerto: ',app.get('port'))
 }
 
 async function main(){
